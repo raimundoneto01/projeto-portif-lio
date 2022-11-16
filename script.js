c69bafc5d480b909ea445f7c0f02f6f9bfbbf0fc
raimundogomes01 <raimundogomes826@gmail.com>
2022-11-16 20:35:46 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,8 @@
+function interropitor(){
+    let lightBody = document.getElementById("body").classList.contains('light');
+    if(lightBody === true){  document.getElementById("body").classList.add("light");
+    document.getElementById("header").classList.add("light")}
+    
+    document.getElementById("body").classList.add("light");
+    document.getElementById("header").classList.add("light")
+}
